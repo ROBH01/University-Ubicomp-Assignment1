18483d22fbda26d132a5e81d8a11dd19b4a41ac4
ROBH01 <SamFisher93gt@>
2020-10-30 15:59:58 +0000

    Implemented a simple 2 stack screens

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Root = createStackNavigator();
+
+const Screen1 = ({ navigation, route }) => (
+  <View style={styles.screen}>
+    <Text style={styles.title}>Screen 1</Text>
+    <Button title="Go to screen 2" onPress={() => navigation.push("Screen2")} />
+  </View>
+);
+const Screen2 = ({ navigation, route }) => (
+  <View style={styles.screen}>
+    <Text style={styles.title}>Screen 2</Text>
+    {/* <Button title="Go back" onPress={() => navigation.pop()} /> */}
+  </View>
+);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Root.Navigator>
+        <Root.Screen name="Screen1" component={Screen1} />
+        <Root.Screen name="Screen2" component={Screen2} />
+      </Root.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  screen: {
+    marginTop: 40,
+    alignItems: "center",
+  },
+  title: {
+    padding: 20,
+    fontSize: 42,
   },
 });
