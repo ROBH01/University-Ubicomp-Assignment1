14efabd60e1d569184173983d7885dace13bf599
ROBH01 <SamFisher93gt@>
2020-10-30 13:39:55 +0000

    Added beach data and app colours

--- app/assets/styles/colors.js
+++ app/assets/styles/colors.js
@@ -2,4 +2,11 @@
 
 export default {
   // my colors here
+  white: "#fff",
+  lightgray: "lightgray",
+  blueTutton: "#2196F3",
+  redSemaphor: "#f41f1f",
+  amberSemaphore: "#f4781f",
+  greenSemaphor: "#4ac63f",
+  unavailableGray: "#afafaf",
 };
